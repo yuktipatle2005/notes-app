bc3c47548e1441ce41f24ba954664075e324970a
yukti patle <yuktipatle563@gmail.com>
2026-04-20 18:34:53 +0530

    env setup

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+require("dotenv").config();
 
 const app = express();
 
@@ -8,10 +9,11 @@ app.use(cors());
 app.use(express.json());
 
 // MongoDB connection
-mongoose.connect("mongodb+srv://yukti:12345@cluster0.xxxxx.mongodb.net/notesDB")
+mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("DB Connected"))
   .catch(err => console.log(err));
-app.listen(5000, () => console.log("Server running on 5000"));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on ${PORT}`));
 
 const Note = require("./models/Note");
 
